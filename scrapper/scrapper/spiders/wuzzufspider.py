20137b89f3e465c184b6293c3d66545a7807d0c6
Aml <aml.hassan.esmil@gmail.com>
2024-01-16 02:51:50 +0200

    get job body from job page

--- scrapper/scrapper/spiders/wuzzufspider.py
+++ scrapper/scrapper/spiders/wuzzufspider.py
@@ -10,25 +10,39 @@ class WuzzufspiderSpider(scrapy.Spider):
     "https://wuzzuf.net/search/jobs/?a=navbl%7Cspbl&q=computer%20vision",
     "https://wuzzuf.net/search/jobs/?a=navbl%7Cspbl&q=data%20science"]
     """
-
-    def parse(self, response):
-        jobs_names=response.css('h2.css-m604qf a::text').getall()
-        jobs_locations= response.css('span.css-5wys0k').xpath('string()').getall()
-        jobs_types=response.css('div.css-1lh32fc').xpath('string()').getall()
-        company_names=response.css("a.css-17s97q8::text").getall()
-        job_urls=response.css('h2.css-m604qf a::attr(href)').getall()
-        company_urls=response.css('h2.css-m604qf a')
+    def __init__(self):
+        self.o=[]
+        self.jobs_names=0
+        self.jobs_locations=0
+        self.jobs_typesxpath=0
+        self.company_namest=0
+        self.job_urls=0
+        self.company_urls=0        
         
-        for i in range(len(jobs_names)):
+    def parse(self, response):
+        self.jobs_names=response.css('h2.css-m604qf a::text').getall()
+        self.jobs_locations= response.css('span.css-5wys0k').xpath('string()').getall()
+        self.jobs_types=response.css('div.css-1lh32fc').xpath('string()').getall()
+        self.company_names=response.css("a.css-17s97q8::text").getall()
+        self.job_urls=response.css('h2.css-m604qf a::attr(href)').getall()
+        self.company_urls=response.css('h2.css-m604qf a')
+        for url in self.job_urls:
+            yield scrapy.Request(url, callback=self.parse_job_page)
+        next_page = response.css('[rel="next"] ::attr(href)').get()
+        if next_page is not None:
+            next_page_url = next_page
+            yield response.follow(next_page_url, callback=self.parse)
+    
+    def parse_job_page(self,response):
+        job_body=response.css("section.css-ghicub").get()
+        for i in range(len(self.jobs_names)):
             yield{
-                    'name':jobs_names[i],
-                    'job_location' :jobs_locations[i],
-                    'job_type' : str(jobs_types[i]).split("}")[-1],
-                    'company_name': company_names[i],
-                    'job_url': job_urls[i],
-                    'company_url': company_urls[i].attrib['href']
+                    'name':self.jobs_names[i],
+                    'job_location' :self.jobs_locations[i],
+                    'job_type' : str(self.jobs_types[i]).split("}")[-1],
+                    'company_name': self.company_names[i],
+                    'job_url': self.job_urls[i],
+                    'company_url': self.company_urls[i].attrib['href'],
+                    "job_body":job_body
                 }
-            next_page = response.css('[rel="next"] ::attr(href)').get()
-            if next_page is not None:
-                next_page_url = next_page
-                yield response.follow(next_page_url, callback=self.parse)
+           
